18b9f56be444ef8632627b93f608ef1660ce3fc8
jgch88 <radium.laboratories@gmail.com>
2019-03-04 00:23:27 +0800

    red: added precondition to check for valid positions for a 3x3 board

--- domain/test/Board.test.js
+++ domain/test/Board.test.js
@@ -19,11 +19,23 @@ describe('board can be initialised to m x n', () => {
 });
 
 describe('placeSymbol() preconditions', () => {
-  it ('symbol must be a string', () => {
+  it('symbol must be a string', () => {
     const board = new Board();
     expect(() => {
       board.placeSymbol(1, 0)
     }).toThrow('Use a string for a symbol.');
   })
 
+  it('position must be between 0 and 8 inclusive (for a 3x3 board)', () => {
+    const board = new Board();
+    expect(() => {
+      board.placeSymbol('x', -1);
+    }).toThrow('You can only place symbols at positions 0 to 8.');
+    expect(() => {
+      board.placeSymbol('x', 9);
+    }).toThrow('You can only place symbols at positions 0 to 8.');
+    expect(() => {
+      board.placeSymbol('o', 1);
+    }).not.toThrow();
+  });
 })
