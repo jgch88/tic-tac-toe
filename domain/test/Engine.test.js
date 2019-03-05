22d3e7d450cb1d086a5a8ad8bf61fc695d7adaea
jgch88 <radium.laboratories@gmail.com>
2019-03-05 13:28:02 +0800

    red: check engine implements registerPlayer(symbol)

--- domain/test/Engine.test.js
+++ domain/test/Engine.test.js
@@ -6,5 +6,11 @@ describe('Engine', () => {
   it('has a board', () => {
     const engine = new Engine();
     expect(engine.board.length).toBe(9);
-  })
-})
+  });
+
+  it('can register players (represented as characters)', () => {
+    const engine = new Engine();
+    engine.registerPlayer('x');
+    expect(engine.players.length).toBe(1);
+  });
+});
