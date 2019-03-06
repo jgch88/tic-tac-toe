344f46580f2702717b56f8fe961ff886f9a4e17e
jgch88 <radium.laboratories@gmail.com>
2019-03-06 17:37:30 +0800

    red+green: added test to check that board renders game in progress and push code coverage to 100%

--- view/test/ConsoleView.test.js
+++ view/test/ConsoleView.test.js
@@ -17,4 +17,25 @@ _|_|_|
 `
     )
   });
+  it('renders a game in progress correctly', () => {
+    let consoleLog = "";
+    console['log'] = jest.fn((input) => consoleLog = input);
+
+    const engine = new Engine();
+    const consoleView = new ConsoleView();
+    engine.registerPlayer('o');
+    engine.registerPlayer('x');
+    engine.play('o', 0);
+    engine.play('x', 1);
+    engine.play('o', 2);
+    engine.play('x', 3);
+    consoleView.render(engine.board);
+    expect(consoleLog).toBe(
+`
+o|x|o|
+x|_|_|
+_|_|_|
+`
+    )
+  });
 });
