16601a52b979df2efcd26059e92e78cdbf4eb0b5
jgch88 <radium.laboratories@gmail.com>
2019-03-09 17:04:34 +0800

    green: added /players route

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
   return res.send();
 });
 
+app.get('/players', (req, res) => {
+  return res.send(Array.from(engine.players));
+});
+
 const server = app.listen(3000, () => {
   console.log('App listening on port:', server.address().port);
 })
