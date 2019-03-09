a4956fa641044dc9ca97792f603cae24bcd0a85e
jgch88 <radium.laboratories@gmail.com>
2019-03-09 18:43:27 +0800

    green: implemented /registerPlayer to throw 409 error along with error message

--- server.js
+++ server.js
@@ -18,7 +18,10 @@ app.post('/registerplayer', (req, res) => {
   try {
     engine.registerPlayer(req.body.symbol)
   } catch (e) {
-    console.log(e);
+    res.status(409);
+    return res.send({
+      message: e.toString(),
+    });
   }
   return res.send(Array.from(engine.players));
 })
@@ -27,15 +30,16 @@ app.get('/hardreset', (req, res) => {
   try{
     engine.hardReset();
   } catch (e) {
-    console.log(e);
   }
   return res.send({
     message: 'Game has reset!'
   });
 })
 
+/*
 const server = app.listen(3000, () => {
   console.log('App listening on port:', server.address().port);
 })
+*/
 
 module.exports = app;
--- server.test.js
+++ server.test.js
@@ -1,65 +1,60 @@
 import app from './server';
 const request = require('supertest');
 
-describe('server', () => {
-  describe('api routes', () => {
-    it('root response returns game status', async () => {
-      const response = await request(app).get('/');
-      expect(response.statusCode).toBe(200);
-    });
+// Had to do this instead because jest can't run sequential testing and server is a stateful singleton
+it('end to end testing', async () => {
+  // root response returns game status
+  let response = await request(app).get('/');
+  expect(response.statusCode).toBe(200);
+  // /players returns a JSON array
+  response = await request(app).get('/players');
+  expect(response.body).toEqual([]);
 
-    it('/players returns a JSON array', async () => {
-      const response = await request(app).get('/players');
-      expect(response.body).toEqual([]);
-    });
-
-    describe('/hardreset', async () => {
-      await request(app)
-        .post('/registerplayer')
-        .send({
-          symbol: 'x'
-        })
-        .set('Accept', 'application/json')
-      let response = await request(app).get('/players');
-      expect(response.body).toEqual(['x']);
-
-      response = await request(app).get('/hardreset');
-      expect(response.statusCode).toBe(200);
-      expect(response.body).toEqual({
-        message: 'Game has reset!',
-      });
-
-      response = await request(app).get('/players');
-      expect(response.body).toEqual([]);
+  // players can register
+  await request(app)
+    .post('/registerplayer')
+    .send({
+      symbol: 'y'
     })
+    .set('Accept', 'application/json')
+  response = await request(app).get('/players');
+  expect(response.body).toEqual(['y']);
 
-    describe('POST /registerPlayer', () => {
-      it('can add a player to the game', async () => {
-        const response = await request(app)
-          .post('/registerplayer')
-          .send({
-            symbol: 'x'
-          })
-          .set('Accept', 'application/json')
-        expect(response.body).toEqual(['x']);
-      });
+  // players can reset the game
+  response = await request(app).get('/hardreset');
+  expect(response.statusCode).toBe(200);
+  expect(response.body).toEqual({
+    message: 'Game has reset!',
+  });
+  response = await request(app).get('/players');
+  expect(response.body).toEqual([]);
 
-      it('adding two players of the same symbol shows an error', async () => {
-        const response = await request(app)
-          .post('/registerplayer')
-          .send({
-            symbol: 'o'
-          })
-          .set('Accept', 'application/json')
-        expect(response.body).toEqual(['o']);
-        const error = await request(app)
-          .post('/registerplayer')
-          .send({
-            symbol: 'o'
-          })
-          .set('Accept', 'application/json')
-        expect(error.statusCode).toBe(409);
-      });
-    });
+  response = await request(app)
+    .post('/registerplayer')
+    .send({
+      symbol: 'x'
+    })
+    .set('Accept', 'application/json')
+  expect(response.body).toEqual(['x']);
+  
+  await request(app).get('/hardreset');
+
+  response = await request(app)
+    .post('/registerplayer')
+    .send({
+      symbol: 'o'
+    })
+    .set('Accept', 'application/json')
+  expect(response.body).toEqual(['o']);
+  // players registering duplicate symbol will get an error
+  let error = await request(app)
+    .post('/registerplayer')
+    .send({
+      symbol: 'o'
+    })
+    .set('Accept', 'application/json')
+  expect(error.statusCode).toBe(409);
+  expect(error.body).toEqual({
+    message: `Error: 'o' is already registered.`,
   });
 });
